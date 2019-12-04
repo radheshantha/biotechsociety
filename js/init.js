8d38537429291644154ce9c211829d08188b15e8
saaa2 <56346379+saaa2@users.noreply.github.com>
2019-12-04 09:21:53 +0000

    events page!

--- js/init.js
+++ js/init.js
@@ -4,7 +4,7 @@
     Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 skel.init({
-	prefix: '/style.css',
+	prefix: 'css/style',
 	resetCSS: true,
 	boxModel: 'border',
 	grid: {
